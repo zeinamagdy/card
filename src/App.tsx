04351d1fe61a4aa001b300acbd031bf4a8c82d6a
zeinamagdy <zainabmagdy8@gmail.com>
2025-09-02 11:14:20 +0200

    responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import qrCode from "./assets/qr-code.png";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
-      <div className="card">
-        <img src={qrCode} className="card-qr-code" alt="qr-code" />
-        <div className="card-text">
-          <h1>Improve your front-end skills by building projects</h1>
-          <p>
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
-          </p>
-        </div>
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+import qrCode from "../assets/qr-code.png";
+import "./Card.css";
+
+function Card() {
+  return (
+    <>
+      <div className="card">
+        <img src={qrCode} className="card-qr-code" alt="qr-code" />
+        <div className="card-text">
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Card;
